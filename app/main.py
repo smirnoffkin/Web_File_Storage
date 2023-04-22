15ba4aab0990178de6451df914902d586db06409
smirnoffkin <vinnycontest@gmail.com>
2023-04-22 22:57:47 +0300

    Added CORS Middleware

--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from create_db import create_database
 from routers import delete, nodes, imports
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(imports.router)
 app.include_router(nodes.router)
